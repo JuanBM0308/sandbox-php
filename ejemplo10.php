92d1240caeb7cd3b194033a6ea4569a3bde32adf
JuanBarbosaMora <portelnom0308@gmail.com>
2025-08-02 18:38:48 -0500

    ADD: Ej 10 -> operadores logicos

---
+++ ejemplo10.php
@@ -0,0 +1,42 @@
+<?php
+
+// TODO: Operadores logicos
+/*
+    && -> AND
+    || -> OR
+*/
+
+if ($_POST) {
+    $valorA = $_POST['valorA'];
+    $valorB = $_POST['valorB'];
+
+    
+    if ($valorA > $valorB) {
+        echo $valorA." es mayor que ".$valorB;
+    } else {
+        echo $valorB." es mayor que ".$valorA;
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="ejemplo10.php" method="post">
+        valor A:
+        <input type="text" name="valorA" id="">
+        <br/>
+        valor B:
+        <input type="text" name="valorB" id="">
+        <br/>
+
+        <input type="submit" value="calcular">
+    </form>
+</body>
+</html>
