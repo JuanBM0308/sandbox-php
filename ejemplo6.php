c6fbc83a9b958c730b1325484546fdcddc3da701
JuanBarbosaMora <portelnom0308@gmail.com>
2025-08-02 16:53:30 -0500

    ADD: Ej 6 -> Tipos de datos

---
+++ ejemplo6.php
@@ -0,0 +1,8 @@
+<?php
+    $edad = 21; // Entero
+    $nombre = "Pepe"; // String
+    $altura = 1.75; // Flotante
+    $activo = true; // Boleano
+
+    echo "Nombre: ".$nombre." edad del paciente ".$edad." altura del paciente ".$altura.", Servicio de EPS: ".$activo;
+?>
