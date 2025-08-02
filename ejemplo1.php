052e0e40e06cc1052cddff83be7203ca241f0ca5
JuanBarbosaMora <portelnom0308@gmail.com>
2025-08-02 16:12:43 -0500

    ADD: Ej 1

---
+++ ejemplo1.php
@@ -0,0 +1,12 @@
+<?php
+
+// Imprime solo valores tipo String
+echo "Hola desde echo";
+
+// Imprime informacion sin importar el formato
+print_r("Hola desde print_r");
+
+// Imprime informacion
+print("Hola desde print");
+
+?>
