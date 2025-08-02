511d03332981d00fe9763f65d0d71e0058518ec6
JuanBarbosaMora <portelnom0308@gmail.com>
2025-08-02 18:08:03 -0500

    ADD: Ej 7 -> constantes php

---
+++ ejemplo7.php
@@ -0,0 +1,17 @@
+<?php
+
+    $edad = 12; // Variable
+
+    echo $edad."<br/>";
+
+    $edad = 50; // Variable -> es posible cambiar el valor
+
+    echo $edad."<br/>";
+
+    // ! Definir constantes
+    define("NOMBRE", "Pedro");
+
+    // ! Imprimir constante
+    echo NOMBRE;
+
+?>
