dfda8caf393db7907f013ee1731f0a6d61dd26a4
JuanBarbosaMora <portelnom0308@gmail.com>
2025-08-02 20:30:51 -0500

    ADD: obtener la informacion / data de la base de datos

--- conexionDB.php
+++ conexionDB.php
@@ -10,9 +10,23 @@ try {
     $conection = new PDO("mysql:host=$server;dbname=album_db", $user, $password);
     $conection -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $sql = "INSERT INTO `fotos` (`id`, `nombre`, `url`) VALUES (NULL, 'viaje a barranquilla', 'memory_baranquilla.jpg')";
+    // $sql = "INSERT INTO `fotos` (`id`, `nombre`, `url`) VALUES (NULL, 'viaje a barranquilla', 'memory_baranquilla.jpg')";
+    $sql = "SELECT * FROM `fotos`";
 
-    $conection -> exec($sql);
+    // ? Prepara la consulta
+    $sentencia = $conection -> prepare($sql);
+
+    // ? Ejecuta la consulta
+    $sentencia -> execute();
+
+    // ? Busca dentro de los resultados
+    $resultado = $sentencia -> fetchAll();
+
+    // ? Imprime los resultados
+    //print_r($resultado);
+    foreach($resultado as $foto) {
+        print_r($foto);
+    }
 
     echo "Conexion a la base de datos establecida";
 
